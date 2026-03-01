9a083e6d1e258ed5885bf9eeb597ff2ccb9e7df9
rauti1022 <r.1022kakutani@icloud.com>
2026-03-01 11:54:18 +0900

    Fix;1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedback,Modal } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { use, useState } from 'react';
 
 export default function App() {
+  const [workTime, setWorkTime] = useState(20)
+  const [breakTime, setBreakTime] = useState(10)
+  const [showWorkPicker, setShowWorkPicker] = useState(false)
+  const [showBreakPicker, setShowBreakPicker] = useState(false)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* 運動 */}
+      <View>
+        <Text>運動時間{workTime}分</Text>
+        {showWorkPicker &&
+          <View onStartShouldSetResponder={() => true}>
+            <Picker
+              selectedValue={workTime}
+              onValueChange={v => setWorkTime(v)}
+              style={styles.picker}>
+              <Picker.Item label='10' value={10}/>
+              <Picker.Item label='20' value={20}/>
+              <Picker.Item label='30' value={30}/>
+              <Picker.Item label='40' value={40}/>
+            </Picker>
+          </View>
+        }
+      </View>
+
+      {/* 休憩 */}
+      <View>
+        <Text>休憩時間{breakTime}分</Text>
+        {showBreakPicker &&
+          <View onStartShouldSetResponder={() => true}>
+            <Picker
+              selectedValue={breakTime}
+              onValueChange={v => setBreakTime(v)}
+              style={styles.picker}>
+              <Picker.Item label='10' value={10}/>
+              <Picker.Item label='20' value={20}/>
+              <Picker.Item label='30' value={30}/>
+              <Picker.Item label='40' value={40}/>
+            </Picker>
+          </View>
+        }
+      </View>
     </View>
   );
 }
@@ -16,5 +54,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  timeSelecter: {
+    flexDirection: 'row',
+  },
+  picker: {
+    height: 50,
+    width: 100,
+  },
+  touchable: {
+    borderWidth: 2,
+    borderColor: 'red',
+    padding: 8,
   },
 });
